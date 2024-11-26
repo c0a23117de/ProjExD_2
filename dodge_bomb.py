25437356519152183fe931a548633b6c87c8dd15
c0a23117 <c0a23117de@edu.teu.ac.jp>
2024-11-26 15:12:07 +0900

    画面反射

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -9,6 +9,13 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 DELTA = {pg.K_UP:(0,-5),pg.K_DOWN:(0,+5),pg.K_LEFT:(-5,0),pg.K_RIGHT:(+5,0)}
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -22,13 +29,18 @@ def main():
     bb_img.set_colorkey((0, 0, 0))
     bb_rct = kk_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):
+            print("ゲームオーバー")
+            return  # ゲームオーバー
         screen.blit(bg_img, [0, 0]) 
+
 
 
 
@@ -39,7 +51,17 @@ def main():
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
